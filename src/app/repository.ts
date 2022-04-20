5dfbda2aac7ff4657ddd8a356216f72bdf61a937
chanaiagwata <chanai.agwata@student.moringaschool.com>
2022-04-20 09:02:32 +0300

    Auto-generated commit [ci skip]

--- src/app/repository.ts
+++
@@ -1,9 +0,0 @@
-export class Repository {
-    name!: string;
-    description!:string
-    username!:string;
-    created_at!:string;
-    forks_count!:string;
-    stargazers_count!:any;
-    language!:string;
-}
